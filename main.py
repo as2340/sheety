1476cc619a9498947d7851aaf53abedce3bd0db1
as2340 <77592730+as2340@users.noreply.github.com>
2022-10-25 22:43:56 +0100

    add row to Google Sheet via Sheety API call. next need to extract data from Nutritionix...

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import json
 import requests
+import datetime as dt
 
 EXERCISE_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
@@ -18,8 +19,8 @@ body = {
 }
 
 response = requests.post(url=EXERCISE_ENDPOINT, headers=headers, json=body)
-data = response.text # JSON string to use eg in jsonviewer.stack.hu
-data_dict = json.loads(data) # equivalent to response.json()
+data = response.text  # JSON string to use eg in jsonviewer.stack.hu
+data_dict = json.loads(data)  # equivalent to response.json()
 
 print(response.status_code)
 print(type(data_dict))
@@ -27,3 +28,25 @@ print(data_dict["exercises"][0]["name"])
 print(data_dict["exercises"][0]["nf_calories"])
 
 print(type(json.dumps(data_dict)))
+
+# add a row to Sheety using a POST request
+
+SHEET_ENDPOINT = os.environ["SHEET_ENDPOINT"]
+
+now = dt.datetime.now()
+
+
+workout_data = {
+    "workout": {
+        "date": now.strftime("%d/%m/%Y"),
+        "time": now.strftime("%H:%M"),
+        "exercise": "Running",
+        "duration": 10,
+        "calories": 114,
+    }
+}
+
+response = requests.post(url=SHEET_ENDPOINT, json=workout_data)
+response.raise_for_status()
+print(response.status_code)
+print(response.text)
